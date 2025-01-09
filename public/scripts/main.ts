77022f32fd496f6c4dc1b64e11e0e625b2d6389c
yamoo9 <yamoo9@naver.com>
2025-01-09 12:55:24 +0900

    [수정] 코드 정리

--- public/scripts/main.ts
+++ public/scripts/main.ts
@@ -3,24 +3,19 @@ import { Greeting } from './greeting';
 // 인사말 리스트(목록) 배열
 const greetingList = Object.values(Greeting);
 
-// 렌더링 함수
-function render() {
-  // 랜덤 인사말 설정
-  const message = greetingList[Math.floor(Math.random() * greetingList.length)];
-  // 화면에 렌더링
-  heading && (heading.textContent = message);
-}
-
-// 대상 참조
+// 제어 대상 참조
 const heading = document.querySelector('h1');
 
-// 버튼 참조
+// 제어 버튼 참조
 const changeButton = document.querySelector('[type="button"]');
 
 // 버튼에 이벤트 바인딩
 changeButton?.addEventListener('click', handleChangeGreetMessage);
 
-// 이벤트 핸들러 (로직 작성)
+// 이벤트 핸들러
 function handleChangeGreetMessage() {
-  render();
+  // 랜덤 인사말 설정
+  const message = greetingList[Math.floor(Math.random() * greetingList.length)];
+  // 화면에 렌더링
+  heading && (heading.textContent = message);
 }
