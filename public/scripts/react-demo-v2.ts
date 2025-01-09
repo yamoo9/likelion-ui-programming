c14b83e09dfda5f0a26974ffd23618f3c3dfd66b
yamoo9 <yamoo9@naver.com>
2025-01-09 15:56:13 +0900

    [정리] 주석 제거

--- public/scripts/react-demo-v2.ts
+++ public/scripts/react-demo-v2.ts
@@ -4,7 +4,11 @@
 import React from '../lib/react.js';
 import ReactDOM from '../lib/react-dom/client.js';
 
-let heading = React.createElement('h1', {}, '안녕!');
+let heading = React.createElement(
+  'h1', 
+  {}, 
+  '안녕!'
+);
 
 const changeButton = React.createElement(
   'button',
@@ -12,8 +16,15 @@ const changeButton = React.createElement(
   '인사말'
 );
 
+// Namespace.module
+// React.Fragment (function)
+
+// 의미 없는 <div> 대신에 <React.Fragment>를 사용
+// 의미 없는 구조는 실제 DOM에 렌더링 되지 않아요!!!
+// console.log(React.Fragment);
+
 const wrapper = React.createElement(
-  React.Fragment,
+  React.Fragment, // type: HTMLTagNameString(e.g: 'a', 'table', ...) / Buit-in Component
   null,
   heading,
   changeButton
