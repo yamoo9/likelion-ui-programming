a7465d84fee285ebb17ad428b3262203c993a3ef
yamoo9 <yamoo9@naver.com>
2025-01-09 13:16:52 +0900

    [실습] JavaScript 마크업 & 이벤트 바인딩 / DOM 마운트

--- public/scripts/main.ts
+++ public/scripts/main.ts
@@ -1,13 +1,22 @@
 import { Greeting } from './greeting';
 
 // 인사말 리스트(목록) 배열
-const greetingList = Object.values(Greeting);
+const greetingList = Object.values(Greeting); // values
+const greetingKeyList = Object.keys(Greeting); // keys
+const greetingEntryList = Object.entries(Greeting); // [[key, value], ...]
 
-// 제어 대상 참조
-const heading = document.querySelector('h1');
+// JavaScript Markup and Event Binding & Mount DOM Element Node
 
-// 제어 버튼 참조
-const changeButton = document.querySelector('[type="button"]');
+// 제어 대상 생성 o / 참조 x
+// const heading = document.querySelector('h1');
+const heading = document.createElement('h1');
+heading.textContent = greetingList.at(0) as string;
+
+// 제어 버튼 생성 o / 참조 x
+// const changeButton = document.querySelector('[type="button"]');
+const changeButton = document.createElement('button');
+changeButton.setAttribute('type', 'button');
+changeButton.textContent = '인사말';
 
 // 버튼에 이벤트 바인딩
 changeButton?.addEventListener('click', handleChangeGreetMessage);
@@ -19,3 +28,9 @@ function handleChangeGreetMessage() {
   // 화면에 렌더링
   heading && (heading.textContent = message);
 }
+
+// 마운트(mount)할 DOM 엘리먼트 노드를 참조
+const app = document.getElementById('app');
+
+// 동적으로 생성된 엘리먼트 노드를 참조한 DOM 노드에 마운트
+app?.prepend(heading, changeButton);
