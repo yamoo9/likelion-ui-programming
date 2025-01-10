b83ca69220c825bcb8eda66760c5c9bc1131e5a5
yamoo9 <yamoo9@naver.com>
2025-01-10 13:16:19 +0900

    [실습] Calcurator 컴포넌트 part 1.

--- public/components/app.ts
+++ public/components/app.ts
@@ -1,8 +1,10 @@
 import React from '../lib/react.js';
+import Calculator from './calculator';
 
 function App() {
   return React.createElement('div', {
     className: 'app',
+    children: React.createElement(Calculator),
   });
 }
 
---
+++ public/components/calculator.ts
@@ -0,0 +1,9 @@
+import React from '../lib/react.js';
+
+function Calculator() {
+  return React.createElement('div', {
+    className: 'calculator',
+  });
+}
+
+export default Calculator;
