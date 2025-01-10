cfa97b380cc01e632d189030b70ded4959e2f448
yamoo9 <yamoo9@naver.com>
2025-01-10 09:22:58 +0900

    [정리] React 엘리먼트와 컴포넌트
    - React 엘리먼트는 요소
    - React 컴포넌트는 React 엘리먼트를 재사용하기 위한 수단(함수)

--- public/scripts/react-demo-v3.ts
+++ public/scripts/react-demo-v3.ts
@@ -23,3 +23,42 @@ const rootElement = document.getElementById('react');
 const reactDomRoot = ReactDOM.createRoot(rootElement);
 
 reactDomRoot.render(wrapper);
+
+// 코드(React 엘리먼트)를 재사용하자
+// let a = 1;
+// let b = 10;
+// let c = a + b;
+
+{
+  // a
+  const heading = React.createElement('h1', {}, '안녕!');
+
+  // b
+  const changeButton = React.createElement(
+    'button',
+    { type: 'button' },
+    '인사말'
+  );
+
+  // c
+  const wrapper = React.createElement(
+    React.Fragment,
+    null,
+    heading,
+    changeButton
+  );
+}
+
+// 재사용 메커니즘 : 클래스(x) 또는 함수(v)
+// 리액트 월드 재사용 메커니즘 : 컴포넌트(함수)
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(add(1, 10));
+
+// a = 11;
+// b = 210;
+// c = a + b;
+
+console.log(add(11, 210));
