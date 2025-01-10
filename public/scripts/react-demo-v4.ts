a3ff879213e27a93cd4fbf9d12001f2aa27e0f99
yamoo9 <yamoo9@naver.com>
2025-01-10 09:42:46 +0900

    [실습] React 컴포넌트 재사용

--- public/scripts/react-demo-v4.ts
+++ public/scripts/react-demo-v4.ts
@@ -4,7 +4,19 @@
 import React from '../lib/react.js';
 import ReactDOM from '../lib/react-dom/client.js';
 
+// React 엘리먼트
 const heading = React.createElement('h1', {}, '안녕!');
+
+// React 컴포너트(이름 작성 규칙 : PascalCase)
+function HeadingOne() {
+  // React 엘리먼트 (생성 후) 반환
+  return React.createElement('h1', { children: '안녕!' });
+}
+
+for (let i = 0; i < 5; ++i) {
+  // 컴포넌트를 사용해 React 엘리먼트 재사용할 수 있음!
+  console.log(React.createElement(HeadingOne));
+}
 
 const changeButton = React.createElement(
   'button',
--- public/scripts/react-demo-v5.ts
+++ public/scripts/react-demo-v5.ts
@@ -4,10 +4,12 @@
 import React from '../lib/react.js';
 import ReactDOM from '../lib/react-dom/client.js';
 
+// Heading 컴포넌트
 function Heading() {
   return React.createElement('h1', {}, '안녕!');
 }
 
+// ChangeButton 컴포넌트
 function ChangeButton() {
   return React.createElement('button', { type: 'button' }, '인사말');
 }
@@ -17,6 +19,16 @@ function Wrapper() {
     React.Fragment,
     null,
     React.createElement(Heading),
+    React.createElement(ChangeButton),
+    React.createElement(Heading),
+    React.createElement(ChangeButton),
+    React.createElement(Heading),
+    React.createElement(ChangeButton),
+    React.createElement(Heading),
+    React.createElement(ChangeButton),
+    React.createElement(Heading),
+    React.createElement(ChangeButton),
+    React.createElement(Heading),
     React.createElement(ChangeButton)
   );
 }
@@ -24,4 +36,5 @@ function Wrapper() {
 const rootElement = document.getElementById('react');
 const reactDomRoot = ReactDOM.createRoot(rootElement);
 
+// React Component -> Creating React Element -> DOM Rendering
 reactDomRoot.render(React.createElement(Wrapper));
