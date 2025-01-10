c58662cad494125ee5c5d4e875573486b68e7832
yamoo9 <yamoo9@naver.com>
2025-01-10 13:07:19 +0900

    [실습] App 컴포넌트

---
+++ public/components/app.ts
@@ -0,0 +1,9 @@
+import React from '../lib/react.js';
+
+function App() {
+  return React.createElement('div', {
+    className: 'app',
+  });
+}
+
+export default App;
---
+++ public/scripts/main.ts
@@ -0,0 +1,7 @@
+import React from '../lib/react.js';
+import ReactDOM from '../lib/react-dom/client.js';
+import App from '../components/app';
+
+ReactDOM.createRoot(document.getElementById('react')).render(
+  React.createElement(App)
+);
