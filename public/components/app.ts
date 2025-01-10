4dce7284bdee7cec66cc7ccf3908cec5729700f6
yamoo9 <yamoo9@naver.com>
2025-01-10 13:46:58 +0900

    [실습] Calcurator 컴포넌트 part 2.
    - 컴포넌트 속성(props)에 따른 출력 변경 구현

--- public/components/app.ts
+++ public/components/app.ts
@@ -4,7 +4,15 @@ import Calculator from './calculator';
 function App() {
   return React.createElement('div', {
     className: 'app',
-    children: React.createElement(Calculator),
+    children: [
+      React.createElement(Calculator),
+      React.createElement(Calculator, {
+        operator: '-',
+      }),
+      React.createElement(Calculator, {
+        elements: [12, 2],
+      }),
+    ],
   });
 }
 
--- public/components/calculator.ts
+++ public/components/calculator.ts
@@ -1,9 +1,40 @@
 import React from '../lib/react.js';
 
-function Calculator() {
-  return React.createElement('div', {
-    className: 'calculator',
-  });
+// HTML = Hyper Text Markup Language
+// h = hyperscript (JavaScript Markup)
+const { createElement: h } = React;
+
+const operations = {
+  '+': (left: number, right: number): number => left + right,
+  '-': (left: number, right: number): number => left - right,
+  '*': (left: number, right: number): number => left * right,
+  '/': (left: number, right: number): number => left / right,
+};
+
+function Calculator(props: { elements?: [number, number]; operator: string }) {
+  const operator = props.operator ?? '+';
+
+  let left = 0;
+  let right = 0;
+
+  if (props.elements) {
+    [left, right] = props.elements;
+  }
+
+  const outputValue: number = operations[operator](left, right);
+
+  return h(
+    'div',
+    {
+      className: 'calculator',
+    },
+    h(
+      'code',
+      null,
+      `${left} ${operator} ${right} = `,
+      h('output', null, outputValue)
+    )
+  );
 }
 
 export default Calculator;
