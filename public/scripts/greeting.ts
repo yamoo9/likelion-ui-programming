06d37e86abab5b2877104d66d7299f62f143357d
yamoo9 <yamoo9@naver.com>
2025-01-10 10:31:43 +0900

    [실습] 컴포넌트 속성(props) 전달

--- public/scripts/greeting.ts
+++ public/scripts/greeting.ts
@@ -1,10 +1,12 @@
 export enum Greeting {
   KO = '안녕!',
-	EN = 'Hello',
-	JP = 'こんにちは',
-	CN = '你好。',
-	ES = 'Hola',
-	FR = 'Bonjour',
-	IT = 'Buon giorno',
-	RU = 'ДРАВСТВУЙТЕ'
+  EN = 'Hello',
+  JP = 'こんにちは',
+  CN = '你好。',
+  ES = 'Hola',
+  FR = 'Bonjour',
+  IT = 'Buon giorno',
+  RU = 'ДРАВСТВУЙТЕ',
 }
+
+export type GreetingKey = keyof typeof Greeting;
--- public/scripts/react-demo.ts
+++ public/scripts/react-demo.ts
@@ -3,20 +3,21 @@
 /* -------------------------------------------------------------------------- */
 import React from '../lib/react.js';
 import ReactDOM from '../lib/react-dom/client.js';
+import { Greeting } from './greeting';
 
-function Heading() {
-  return React.createElement('h1', {}, '안녕!');
+function Heading(props: { greetingMessage: string }) {
+  return React.createElement('h1', {}, props.greetingMessage);
 }
 
 function ChangeButton() {
   return React.createElement('button', { type: 'button' }, '인사말');
 }
 
-function Wrapper() {
+function Wrapper(props: { message: string }) {
   return React.createElement(
     React.Fragment,
     null,
-    React.createElement(Heading),
+    React.createElement(Heading, { greetingMessage: props.message }),
     React.createElement(ChangeButton)
   );
 }
@@ -24,4 +25,4 @@ function Wrapper() {
 const rootElement = document.getElementById('react');
 const reactDomRoot = ReactDOM.createRoot(rootElement);
 
-reactDomRoot.render(React.createElement(Wrapper));
+reactDomRoot.render(React.createElement(Wrapper, { message: Greeting.ES }));
