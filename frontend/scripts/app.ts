88282d88f6fd8af954fc69ef194ad8783dc4b9ad
yamoo9 <yamoo9@naver.com>
2025-01-13 11:41:35 +0900

    [실습] Switch 컴포넌트 만들기 01
    - 컴포넌트 구성
    - 컴포넌트 속성 설계
    - 컴포넌트 사용 API
    - 컴포넌트가 생성할 마크업은 진행 중...

--- frontend/scripts/app.ts
+++ frontend/scripts/app.ts
@@ -1,11 +1,19 @@
 import React from '../lib/react.js';
+import Switch from '../components/switch';
 
 const h = React.createElement;
 
 function App(): React.ReactNode {
-  return h('div', {
-    style: appStyles,
-  });
+  return h(
+    'div',
+    {
+      lang: 'en',
+      style: appStyles,
+    },
+    h(Switch, { active: true }, 'Wi-Fi'),            // JSX: <Switch active>Wi-Fi</Switch>
+    h(Switch, { active: false }, 'Bluetooth'),       // JSX: <Switch>Bluetooth</Switch>
+    h(Switch, { disabled: true }, 'Reduced Motion'), // JSX: <Switch disabled>Reduced Motion</Switch>
+  );
 }
 
 const appStyles = {
