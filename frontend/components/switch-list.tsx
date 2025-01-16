a889aab3a5729ce3b95a26ad79370b7bc9e267b0
yamoo9 <yamoo9@naver.com>
2025-01-16 11:41:08 +0900

    [실습] 순수하지 않은 행위를 시도
    - 컴포넌트를 2번 렌더링 하는 이유
    - StrictMode를 사용하는 이유

--- frontend/components/switch-list.tsx
+++ frontend/components/switch-list.tsx
@@ -14,67 +14,33 @@ const switchStyles = {
   paddingInlineStart: 0,
 };
 
+// 컴포넌트 속성(props)는 읽기전용(컴포넌트 내부에서 수정하면 안됨)
 function SwitchList({ items }: SwitchListProps) {
-  // 좀 더 복잡한 구조의 상태(객체)
-  const [state, setState] = React.useState({
-    submission: false,
-    review: false,
-    myLevel: true,
+  // 컴포넌트는 순수해야 한다.
+  console.log(items);
+
+  // 순수하지 않은 행위를 시도해봅니다.
+  // 전달받은 컴포넌트 속성 수정 시도
+  items.push({
+    id: 'item-5',
+    children: '컴포넌트 순수성',
+    active: true,
   });
-
-  // 여러 상태를 하나의 상태 업데이트 함수 로직으로 관리
-  const handleStateToggle = (name: string): void => {
-    // JavaScript 객체는 [ 계산된 ] 속성을 가질 수 있다.
-    // let name = 'myLevel';
-    const nextState = {
-      ...state,
-      [name]: !state[name],
-    };
-
-    setState(nextState);
-  };
 
   return (
     <ul className="SwitchList" style={switchStyles}>
-      <li>
-        <Switch
-          active={state.submission}
-          onToggle={() => handleStateToggle('submission')}
-        >
-          과제 제출
-        </Switch>
-      </li>
-      <li>
-        <Switch
-          active={state.review}
-          onToggle={() => handleStateToggle('review')}
-        >
-          의미있는 복습 수행
-        </Switch>
-      </li>
-      <li>
-        <Switch
-          active={state.myLevel}
-          onToggle={() => handleStateToggle('myLevel')}
-        >
-          정확한 나의 수준 진단
-        </Switch>
-      </li>
-
-      {/* 조건부 렌더링(Conditional Rendering) */}
-      {false &&
-        items.map((item) => (
-          <li key={item.id}>
-            <Switch
-              active={item.active}
-              disabled={item.disabled}
-              showOnOffText={item.showOnOffText}
-              onToggle={item.onToggle}
-            >
-              {item.children}
-            </Switch>
-          </li>
-        ))}
+      {items.map((item) => (
+        <li key={item.id}>
+          <Switch
+            active={item.active}
+            disabled={item.disabled}
+            showOnOffText={item.showOnOffText}
+            onToggle={item.onToggle}
+          >
+            {item.children}
+          </Switch>
+        </li>
+      ))}
     </ul>
   );
 }
