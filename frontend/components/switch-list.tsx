ac18bb4bfc8cbea560191f98aa21b4e5ef210f46
yamoo9 <yamoo9@naver.com>
2025-01-16 10:42:59 +0900

    [실습] 컴포넌트 상태 관리 (추가)
    - 상태와 상태 업데이트 로직 구분 (관심사 분리)

--- frontend/components/switch-list.tsx
+++ frontend/components/switch-list.tsx
@@ -20,35 +20,45 @@ function SwitchList({ items }: SwitchListProps) {
   // 어떤 상태? (과제 제출)
   // React의 선언적 API
   // 상태 선언 (WHAT)
+
+  // [관심사] 과제 제출 상태 & 상태 업데이트 로직
   const [submission, setSubmission] = React.useState(false);
+  const handleToggleSubmission = () => {
+    const nextSubmission = !submission;
+    setSubmission(nextSubmission);
+  };
+
+  // [관심사] 복습 수행 상태 & 상태 업데이트 로직
+  const [review, setReview] = React.useState(false);
+  const handleToggleReview = () => {
+    const nextReview = !review;
+    setReview(nextReview);
+  };
+
+  // [관심사] 나의 수준 파악 상태 & 상태 업데이트 로직
+  const [myLevel, setMyLevel] = React.useState(false);
+  const handleToggleMyLevel = () => {
+    const nextMyLevel = !myLevel;
+    setMyLevel(nextMyLevel);
+  };
 
   return (
     <ul className="SwitchList" style={switchStyles}>
       <li>
-        <Switch
-          active={submission}
-          onToggle={() => {
-            // 상태 업데이트 (React에 렌더 요청: trigger a render)
-            const nextSubmission = !submission; // false <=> true
-            setSubmission(nextSubmission);
-          }}
-        >
+        <Switch active={submission} onToggle={handleToggleSubmission}>
           과제 제출
         </Switch>
       </li>
-      {/* 조건부 디스플레이(Conditional Display) */}
-      {items.map((item) => (
-        <li key={item.id} style={{ display: 'none' }}>
-          <Switch
-            active={item.active}
-            disabled={item.disabled}
-            showOnOffText={item.showOnOffText}
-            onToggle={item.onToggle}
-          >
-            {item.children}
-          </Switch>
-        </li>
-      ))}
+      <li>
+        <Switch active={review} onToggle={handleToggleReview}>
+          의미있는 복습 수행
+        </Switch>
+      </li>
+      <li>
+        <Switch active={myLevel} onToggle={handleToggleMyLevel}>
+          정확한 나의 수준 진단
+        </Switch>
+      </li>
 
       {/* 조건부 렌더링(Conditional Rendering) */}
       {false &&
